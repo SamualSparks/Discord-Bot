3c1b3a29446d93a38248ebb5a44a590abd6f74b4
DudsDank <tobywonkanobi@yahoo.com>
2020-12-17 10:37:56 -0700

    Final before test

--- RPi_Files/update.py
+++ RPi_Files/update.py
@@ -1,10 +1,20 @@
 import os
 
-def deleteDir(dir):
+def deleteDir():
+    dir = "Desktop/bot/Bot"
     os.system("rm -rf " + dir)
 
 def mk_bot_dir():
     os.system("mkdir Desktop/bot/Bot")
     
-def cloneRepo(dir,addy):
-    os.system("put clone shit here")
+def cloneRepo():
+    dir = "Desktop/bot/Bot"
+    os.system("cd " + dir)
+    os.system("git clone https://github.com/SamualSparks/Discord-Bot.git")
+
+def run():
+    deleteDir()
+    mk_bot_dir()
+    cloneRepo()
+
+run()
